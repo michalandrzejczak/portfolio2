b225578e553af52d9c411d3df6c98ddacebeb159
Michał Andrzejczak <michal.andrzejcz@gmail.com>
2018-10-05 00:11:54 +0200

    Added all sections, a lot of styling, not paid attention to content yet

--- src/js/animationsModule.js
+++ src/js/animationsModule.js
@@ -4,7 +4,7 @@ import * as ScrollMagic from 'ScrollMagic';
 $(function () {
 	
 	const ScrollMagic = require("scrollmagic");
-	const controller = new ScrollMagic.Controller({addIndicators: true});
+	const controller = new ScrollMagic.Controller();
 
 	var horizontalSlide1 = new TimelineMax()
 	.to("#js-slider1", 1,   {x: "-50%"})	
@@ -18,7 +18,19 @@ $(function () {
 	})
 		.setPin("#js-pin1-wrapper")
 		.setTween(horizontalSlide1)
-		.addIndicators()
+		.addTo(controller);
+	
+	var horizontalSlide2 = new TimelineMax()
+	.to("#js-slider2", 1,   {x: "-50%"})	
+	
+	// second horizontal section
+	new ScrollMagic.Scene({
+		triggerElement: "#js-pin2-wrapper",
+		triggerHook: "onLeave",
+		duration: "200%"
+	})
+		.setPin("#js-pin2-wrapper")
+		.setTween(horizontalSlide2)
 		.addTo(controller);
 
 
---
+++ src/js/perfectScroll.js
@@ -0,0 +1,3 @@
+import PerfectScrollbar from "perfect-scrollbar";
+
+var ps = new PerfectScrollbar(".projects-container");
