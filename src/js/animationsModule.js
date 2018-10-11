a5be65a678cb996ec0d1181ee2b86706b9d350e1
Michał Andrzejczak <michal.andrzejcz@gmail.com>
2018-10-11 22:28:05 +0200

    Added JS handling of scrollmagic mount related to window size

--- src/js/animationsModule.js
+++ src/js/animationsModule.js
@@ -3,36 +3,78 @@ import * as ScrollMagic from 'ScrollMagic';
 
 $(function () {
 	
-	const ScrollMagic = require("scrollmagic");
-	const controller = new ScrollMagic.Controller();
+	let isWidthDesktop,
+			isScrollMagicMounted = false;
 
-	var horizontalSlide1 = new TimelineMax()
-	.to("#js-slider1", 1,   {x: "-50%"})	
+	const checkWindowWidth = () => {
+		if (window.innerWidth > 768) {
+			isWidthDesktop = true;
+		} else {
+			isWidthDesktop = false;
+		}
+	}
 
+	const mountScrollMagic = () => {
+		if (isWidthDesktop && !isScrollMagicMounted) { // mounting ScrollMagic for desktop width
+			
+			const ScrollMagic = require("scrollmagic");
+			var controller = new ScrollMagic.Controller();
 
- 	// first horizontal section
-	new ScrollMagic.Scene({
-		triggerElement: "#js-pin1-wrapper",
-		triggerHook: "onLeave",
-		duration: "200%"
+			const horizontalSlide1 = new TimelineMax()
+			.to("#js-slider1", 1, {
+				x: "-50%"
+			}),
+						horizontalSlide2 = new TimelineMax()
+			.to("#js-slider2", 1, {
+				x: "-50%"
+			});
+
+			isScrollMagicMounted = true;
+			// first horizontal section
+			var scene1 = new ScrollMagic.Scene({
+					triggerElement: "#js-pin1-wrapper",
+					triggerHook: "onLeave",
+					duration: "200%"
+				})
+				.setPin("#js-pin1-wrapper")
+				.setTween(horizontalSlide1)
+				.addTo(controller);
+
+			// second horizontal section
+			var scene2 = new ScrollMagic.Scene({
+					triggerElement: "#js-pin2-wrapper",
+					triggerHook: "onLeave",
+					duration: "200%"
+				})
+				.setPin("#js-pin2-wrapper")
+				.setTween(horizontalSlide2)
+				.addTo(controller);
+		} else if (isWidthDesktop && isScrollMagicMounted) { // doing nothing if width is still desktopish but ScrollMagic already mounted
+			return false
+		} else { // if width <= 768px unmount ScrollMagic
+			controller = null;
+			scene1 = null;
+			scene2 = null;
+			isScrollMagicMounted = false;
+			return false;
+		}
+	}
+
+	checkWindowWidth(); // initial check of window size
+	mountScrollMagic(); // initial check on ScrollMagic mounting
+
+	let timeout = false; // debouncing helper
+
+	window.addEventListener("resize", () => {
+		if (!timeout) {
+			timeout = true;
+			document.location.reload(true);
+			checkWindowWidth();
+			mountScrollMagic();
+			setTimeout(() => {
+				timeout = false;
+			}, 500)
+		}
 	})
-		.setPin("#js-pin1-wrapper")
-		.setTween(horizontalSlide1)
-		.addTo(controller);
-	
-	var horizontalSlide2 = new TimelineMax()
-	.to("#js-slider2", 1,   {x: "-50%"})	
-	
-	// second horizontal section
-	new ScrollMagic.Scene({
-		triggerElement: "#js-pin2-wrapper",
-		triggerHook: "onLeave",
-		duration: "200%"
-	})
-		.setPin("#js-pin2-wrapper")
-		.setTween(horizontalSlide2)
-		.addTo(controller);
-
-
 
 });
